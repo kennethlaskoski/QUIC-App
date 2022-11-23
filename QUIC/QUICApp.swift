ccdca7752ff0ca4ccd7de594ac028d481d30b5ee
Kenneth Laskoski <kenneth@laskoski.com.br>
2022-11-23 10:28:17 -0300

    Initial Commit

---
+++ QUIC/QUICApp.swift
@@ -0,0 +1,17 @@
+//
+//  QUICApp.swift
+//  QUIC
+//
+//  Created by Kenneth Laskoski on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct QUICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
